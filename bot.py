967aa78738cad889808d2946c4c434f604d2cbb0
aldomirjr <108947220+aldomirjr@users.noreply.github.com>
2026-02-18 23:14:38 -0300

    Add birthday notification bot using Telegram API

---
+++ bot.py
@@ -0,0 +1,44 @@
+import requests
+import datetime
+import os
+import pandas as pd
+
+TOKEN = os.getenv("8315623446:AAG7PqkDBZkM6FSqHbUxTtt0N-_MAbp1YSY")
+CHAT_ID = os.getenv("https://t.me/+xwiYQWEP6hdiZDFh")
+
+URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRBuYPsC6XDnfjGLkWF5saaL7wbEFTdRNamIT6vg3i13F3ngEfbP0JnuvHHefvpMMGuOm99yrpG69te/pub?output=csv"
+
+df = pd.read_csv(URL)
+
+# Garantir que Mes e dia sejam número
+df["Mes"] = df["Mes"].astype(int)
+df["dia"] = df["dia"].astype(int)
+
+hoje = datetime.datetime.now()
+mes_atual = hoje.month
+dia_atual = hoje.day
+
+aniversariantes = df[
+    (df["Mes"] == mes_atual) &
+    (df["dia"] == dia_atual)
+]
+
+if not aniversariantes.empty:
+    nomes = "\n".join(
+        [f"• {nome}" for nome in aniversariantes["Nome completo"]]
+    )
+
+    mensagem = f"""
+🎉 Aniversariantes de Hoje 🎉
+
+{nomes}
+
+Que Deus abençoe grandemente esse novo ano! 🙏
+"""
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": mensagem
+    })
